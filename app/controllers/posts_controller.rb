d3d1d4b0f4a36eb8ab6226ddcecca8b3ad3ce95a
shasherazi <hassanrandomz@gmail.com>
2023-06-13 06:14:07 +0500

    Create posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def index
+    # @user = User.find(params[:id])
+    # @posts = @user.posts
+  end
+
+  def show
+    # @user = User.find(params[:id])
+    # @post = Post.find(params[:id])
+  end
+end
