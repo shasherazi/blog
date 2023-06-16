3c6489a35b2136d367bf6282a83a20fc5df6a6ed
shasherazi <hassanrandomz@gmail.com>
2023-06-16 10:19:02 +0500

    Add `new` route for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @user = current_user
+    @comment = Comment.new
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @user = User.find(params[:user_id])
@@ -9,7 +15,7 @@ class CommentsController < ApplicationController
       redirect_to user_post_url(@user, @post), notice: 'Comment created successfully!'
     else
       flash[:errors] = @comment.errors.full_messages
-      redirect_to user_post_url(@user, @post)
+      redirect_to new_user_post_comment_url(@user, @post)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[create]
+      resources :comments, only: %i[create new]
       resources :likes, only: %i[create]
     end
   end
