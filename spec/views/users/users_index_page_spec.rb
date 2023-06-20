fa9ae2f0cb85f4c5222c883e2851555ee2978738
zeff96 <zeffo1975@gmail.com>
2023-06-20 11:33:18 +0300

    Add test validation for user number of posts

--- spec/views/users/users_index_page_spec.rb
+++ spec/views/users/users_index_page_spec.rb
@@ -12,4 +12,9 @@ RSpec.describe 'user index page', type: :feature do
     visit '/'
     expect(page).to have_css("img[src='#{user.photo}']")
   end
+
+  scenario 'display number of post for a given user' do
+    visit '/'
+    expect(page).to have_content(user.posts_counter)
+  end
 end
