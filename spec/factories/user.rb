9213a832e4c7f232254d0142c50cda4674c23248
zeff96 <zeffo1975@gmail.com>
2023-06-20 11:46:21 +0300

    Add post for tests and set association with user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,24 @@ FactoryBot.define do
     name { 'Tom' }
     photo { 'https://unsplash.com/photos/F_-0BxGuVvo' }
     bio { 'Teacher from Mexico.' }
+
+    factory :user_with_posts do
+      transient do
+        post_count { 3 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:post, evaluator.post_count, author: user)
+      end
+    end
+  end
+end
+
+FactoryBot.define do
+  factory :post do
+    title { 'hello there' }
+    text { 'this is my first post' }
+    comments_counter { 0 }
+    likes_counter { 0 }
   end
 end
