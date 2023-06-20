d31ef8de85413a07a723c1759ec5d975f1b5255a
zeff96 <zeffo1975@gmail.com>
2023-06-20 11:50:53 +0300

    Add test validation for user show page profile picture

--- spec/views/users/users_show_page_spec.rb
+++ spec/views/users/users_show_page_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
+user = FactoryBot.create(:user)
+post = FactoryBot.create(:post, author: user)
+
 RSpec.describe 'user show page', type: :feature do
-  
+  scenario 'dislpay user profile picture' do
+    visit user_path(user)
+    expect(page).to have_css("img[src='#{user.photo}']")
+  end
 end
