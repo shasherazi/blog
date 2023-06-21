411c4a2b8c6235dbb0f3cda41e2e9f9ddbed32ed
shasherazi <hassanrandomz@gmail.com>
2023-06-21 11:26:44 +0500

    Update routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
