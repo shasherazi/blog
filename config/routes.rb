b2993f2dad1fc97e5f195405df7fe3ea36913c29
zeff96 <zeffo1975@gmail.com>
2023-06-21 13:35:06 +0300

    Add route for authenticated user to user index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'users#index'
 
+  devise_scope :user do
+    authenticated :user do
+      root to: 'user#index', as: :authenticated_root
+    end
+  end
+
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[create new]
