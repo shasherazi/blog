157fccdf95ee01f4c82c035a4fa9834a16eac6eb
zeff96 <zeffo1975@gmail.com>
2023-06-21 13:54:37 +0300

    Update routes for root

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   # Defines the root path route ("/")
-  root 'users#index'
-
   devise_scope :user do
     authenticated :user do
       root to: 'users#index', as: :authenticated_root
